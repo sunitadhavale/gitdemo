615315a3274e0c3f2536f2bb16349ae13f62c76c
sunitadhavale <sunitadhavale75@hotmail.com>
2023-05-03 13:44:03 +0530

    feature3 changes not reflected in master branch

--- helloworld.sh
+++ helloworld.sh
@@ -3,4 +3,4 @@ echo hello aditi!
 echo new sentence for testing added
 echo new line after 1st commit added
 echo new feature added in branch feature1
-
+echo testing rebase with feature3 so added changes in masterbranch only
